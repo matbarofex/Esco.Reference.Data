9e57f95b41ca6c6711a3a69f042d5ccc180eb2c0
tgullon <tgullon@primary.com.ar>
2024-02-01 11:08:03 -0300

    agrego campos schema-012, falta confirmar factor

--- esco.reference.data.domain/mapping/OpcionesOTC.cs
+++ esco.reference.data.domain/mapping/OpcionesOTC.cs
@@ -18,42 +18,27 @@ namespace ESCO.Reference.Data.Model
     public class OpcionOTC
     {
         public string name { get; set; }
-
         public string type { get; set; }
-
         public bool? active { get; set; }
-
         public OpcionOTCFields fields { get; set; }
-
         public DateTime? updated { get; set; }
-
     }
 
     public class OpcionOTCFields
     {
         public string cfiCode { get; set; }
-
         public string symbol { get; set; }
-
         public string relatedSymbol { get; set; }
-
         public string contractMultiplier { get; set; }
-
         public string maturityDate { get; set; }
-
         public string underlyingSymbol { get; set; }
-
         public string strikePrice { get; set; }
-
         public string securityStatus { get; set; }  
-
         public string currency { get; set; }
-
         public string marketId { get; set; }    
-
         public string country { get; set; }
-
         public string putOrCall { get; set; }
-
+        public string marketSegmentId { get; set; }
+        public string underlyingSecurityId { get; set; }
     }
 }
